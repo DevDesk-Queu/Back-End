eb4d153056db441b826769631ca1bf8a293d988e
MattGThomas <mgthomas0616@gmail.com>
2019-09-17 20:35:04 -0500

    2nd commit

--- api/server.js
+++ api/server.js
@@ -4,6 +4,8 @@ require('dotenv').config()
 const server = express()
 
 server.use(express.json())
+// server.use('/api/users')
+// server.use('/api/tickets')
 
 server.get('/', (req, res) => {
     res.status(200).json({ api:'up' })
