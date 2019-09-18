234c036e5bdfb6f427ff2af57d1bf52308ee4d8f
MattGThomas <mgthomas0616@gmail.com>
2019-09-18 18:51:03 -0500

    added start script

--- api/server.js
+++ api/server.js
@@ -1,17 +1,23 @@
 const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
 require('dotenv').config()
 
 const authRouter = require('../auth/auth-router.js')
 const server = express()
 
 server.use(express.json())
+server.use(helmet())
+server.use(cors())
 
 server.use('/api/auth', authRouter)
 // server.use('/api/users')
 // server.use('/api/tickets')
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api:'up' })
+    res.send(`
+    <h2>DevDesk API<h2>
+    `)
 })
 
 module.exports = server
