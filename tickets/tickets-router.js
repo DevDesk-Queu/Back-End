0734c704ee977d4477b0f06899d732fe709b29a1
MattGThomas <mgthomas0616@gmail.com>
2019-09-19 00:16:32 -0500

    added post to / to add ticket

--- tickets/tickets-router.js
+++ tickets/tickets-router.js
@@ -15,4 +15,27 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', requireUserId, (req, res) => {
+    const ticketInfo = {...req.body}
+    Tickets.addTicket(ticketInfo)
+        .then(ticket => {
+            res.status(201).json(ticket)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: 'there was an error adding the ticket'
+            })
+        })
+})
+
+function requireUserId(req, res, next) {
+    if (!req.body.user_id) {
+        res.status(500).json({
+            message: 'a user_id is required'
+        })
+    } else {
+        next()
+    }
+}
 module.exports = router
