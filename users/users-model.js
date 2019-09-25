a9a1247d18046a47a876d0a56165bfc0941ae01f
MattGThomas <mgthomas0616@gmail.com>
2019-09-24 20:50:36 -0500

    trying to fix error

--- users/users-model.js
+++ users/users-model.js
@@ -28,7 +28,7 @@ function findById(id) {
 async function add(user) {
     return db('users').insert(user)
     .then(ids => {
-        console.log('*************** id:',id)
+        console.log('*************** id:',ids)
         return db('users').where({ id: ids[0]}).first();
 
     })
